53014a266d558edede3c1c6798f9b03c2f5c51af
GabrielGLuna <gabrielunagarcia@hotmail.com>
2023-09-08 14:56:48 -0600

    03 Operadores

---
+++ operadores.js
@@ -0,0 +1,37 @@
+//Operadores matematicos
+
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(1 * 1);
+console.log(1 / 1);
+console.log(1 % 1);
+console.log(1 ** 1);
+console.log(3 ** 3);
+console.log(4 ** 4);
+
+// Mas operadores}
+let a = 10;
+console.log(a);
+a += 10; // a = a + 10
+console.log(a);
+a -= 10; // a = a - 10
+console.log(a);
+a *= 10; // a = a * 10
+console.log(a);
+a /= 10; // a = a / 10
+console.log(a);
+a %= 10; // a = a % 10
+console.log(a);
+
+// Operadores relacionales
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log(10 !== 10);
+console.log(10 === 10);
+
+//Operadores Lógicos
+console.log(true && true);
+console.log(true || true);
+console.log(!true)
